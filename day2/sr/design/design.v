c907248a6fc686d2d68e915b2ed296d39559e8a9
noooah2006 <noahmjose2006@gmail.com>
2026-06-09 22:35:24 +0530

    Update design.v

--- day2/sr/design/design.v
+++ day2/sr/design/design.v
@@ -1,1 +1,54 @@
 
+
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/09/2026 03:11:38 PM
+// Design Name: 
+// Module Name: sr_flipflop
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sr_flipflop(
+    input s, r, rst, clk,
+    output reg q, qbar
+);
+
+
+    always @(posedge clk) begin
+        if (rst) begin
+            q    <= 1'b0;
+            qbar <= 1'b1;
+        end
+        else if (s == 0 && r == 0) begin
+            q    <= q;
+            qbar <= qbar;
+        end
+        else if (s == 0 && r == 1) begin
+            q    <= 1'b0;
+            qbar <= 1'b1;
+        end
+        else if (s == 1 && r == 0) begin
+            q    <= 1'b1;
+            qbar <= 1'b0;
+        end
+        else if (s == 1 && r == 1) begin
+            q    <= 1'bx;   
+            qbar <= 1'bx;   
+        end
+    end
+
+endmodule
