c6a4e9e1e9d3bb87a5f80085b34be01330a93ace
noooah2006 <noahmjose2006@gmail.com>
2026-06-09 22:36:07 +0530

    Update tb.v

--- day2/sr/tb/tb.v
+++ day2/sr/tb/tb.v
@@ -1,1 +1,67 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/09/2026 03:15:18 PM
+// Design Name: 
+// Module Name: sr_flipflop_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+
+module sr_flipflop_tb();
+
+    reg s_tb, r_tb, rst_tb, clk_tb;
+    wire q_tb, qbar_tb;
+
+    
+    sr_flipflop dut (s_tb, r_tb, rst_tb, clk_tb, q_tb, qbar_tb);
+
+    initial begin
+        {s_tb, r_tb, rst_tb, clk_tb} = 4'b0000; 
+    end
+
+    
+    always #5 clk_tb = ~clk_tb;
+
+    initial begin
+        rst_tb = 1;
+        #10;
+        
+        rst_tb = 0;
+        s_tb = 0;
+        r_tb = 0;
+        #10;
+        
+        s_tb = 0;
+        r_tb = 1;
+        #10;
+        
+        s_tb = 1;
+        r_tb = 0;
+        #10;
+        
+        s_tb = 1;
+        r_tb = 1;
+        #10;
+        
+        s_tb = 1;
+        r_tb = 1;
+        #10;
+        
+        $finish; 
+    end
+
+endmodule
